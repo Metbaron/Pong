8d90366b26fead89ca834ec771c7326676eb6a11
Metbaron <johannesfue@gmx.at>
2020-07-05 10:11:21 +0200

    Added scoring

--- Pong1.py
+++ Pong1.py
@@ -6,6 +6,10 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)  # stops automatic updating
 
+# Score
+score_a = 0
+score_b = 0
+
 # Paddle A
 paddle_a = turtle.Turtle()  # Turtle object
 paddle_a.speed(0)  # speed of animation to max
@@ -32,11 +36,19 @@ ball.shape("square")  # built in shapes
 ball.color("white")
 ball.penup()  # something with lines
 ball.goto(0, 0)  # Navigator
-ball.dx = 0.75  # x-axis movement of ball, new attribute
-ball.dy = 0.75  # y-axis movement of ball
+ball.dx = 0.5  # x-axis movement of ball, new attribute
+ball.dy = 0.5  # y-axis movement of ball
+
+# Pen for keeping score
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()  # no drawing line when pen moves
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
 
 # Game functions
-
 
 def paddle_a_up():
     # consider taking paddle as input
@@ -93,13 +105,21 @@ while True:
         ball.dy *= -1
 
     #  Todo make it so that ball starts from pedal
+    #  Todo create scoring function to update on hit
     if ball.xcor() > 390 :
         ball.goto(0, 0)  # reset ball
         ball.dx *= -1  # reverse direction
+        score_a += 1  # score keeping
+        pen.clear()  #delete previous score
+        pen.write("Player A: {}  Player B: {}".format(score_a,score_b), align="center", font=("Courier", 24, "normal"))
 
     if ball.xcor() < -390 :
         ball.goto(0, 0)
         ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a,score_b), align="center", font=("Courier", 24, "normal"))
+
 
     # Paddle ball collision
     # Collision when ball hits certain x coord and y coord of paddle
